57e7a68c9accea6c4d55f39e6a765681dc6eb15f
PabloViniciusSS <48532322+PabloViniciusSS@users.noreply.github.com>
2024-01-01 18:38:44 -0300

    Initial commit

---
+++ quizz.kt
@@ -0,0 +1,56 @@
+/* 
+Para criar uma lista deixando o código padronizado podemos utilizar o enum
+*/
+enum class Difficulty{
+  EASY, MEDIUM, HARD
+}
+
+/*Tipos genericos é quando você tem varias classes com algumas variaveis iguais e uma ou mais varivel que o tipo vai diferenciar entre elas, para não criar varias classes parecidas, pode usar o genericos para reutilizar uma classe.
+Normalmente se utiliza uma letra maiscula para ser o generico.
+
+*/
+
+/*
+Como a classe Question não realiza ação, apenas dados, ele pode ser definido como classes de dados, fazendo como que haja uma implementação de alguns métodos de modo automático.
+
+* equals();
+* hasCode();
+* toString();
+* compenentN()
+* copy()
+
+Observação: Há classe dados não pode ser abstract, open, sealed ou inner
+ */
+data class Question<T>(
+  val questionText: String,
+  val answer: T, 
+  val difficulty: Difficulty
+)
+
+ 
+
+
+
+/*
+Singleton é um objeto no qual é criando apenas uma instância, esse objeto tem como objtivo não permitir a criação de duas instâncias ao mesmo tempos, podendo assim gerar um bug na hora.
+
+O Singleton não tem um construtor, as propriedades são definidas dentro das chaves, recebem valor inicial
+ */
+
+class Quiz{
+//Ao  utilizar a classe passa o tipo da variavel generico.
+  val question1 = Question<String>("Já houve quantas copas do mundo no Brasil?", "2", Difficulty.MEDIUM)
+  val quetion2 = Question<Bolean>("O céu é verde. Verdadeuiro ou Falso", false, Difficulty.EASY)
+  val question3 = Question<Int>("Quantos dias tem entre as fase da lua?", 28, Difficulty.HARD)
+  println(question1.toString())
+
+companion object StudentProgress{
+    var total: Int = 10
+    var answered: Int = 3
+}
+
+}
+
+fun main() {
+    println("${Quiz.answered} of ${Quiz.total} answered.")
+}
